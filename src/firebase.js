4d2bb6e5bc86270746f832906275c3e3df288bbf
kaung-san-hein <kaungsanhein2019@gmail.com>
2021-11-28 00:04:25 +0630

    [Done]Register Function

---
+++ src/firebase.js
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.API_KEY,
+//   authDomain: process.env.AUTH_DOMAIN,
+//   databaseURL: process.env.DATABASE_URL,
+//   projectId: process.env.PROJECT_ID,
+//   storageBucket: process.env.STORAGE_BUCKET,
+//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//   appId: process.env.APP_ID,
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjGfqwhM6hqnPK41i5019F1YNX-R28nik",
+  authDomain: "chat-app-b4b9a.firebaseapp.com",
+  databaseURL: "https://chat-app-b4b9a.firebaseio.com",
+  projectId: "chat-app-b4b9a",
+  storageBucket: "chat-app-b4b9a.appspot.com",
+  messagingSenderId: "536813550916",
+  appId: "1:536813550916:web:ad591cba078a634fbaab11"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
