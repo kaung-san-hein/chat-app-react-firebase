70a44414597c54c76f33a47694a0fda1768fb784
kaung-san-hein <kaungsanhein2019@gmail.com>
2021-11-29 15:47:22 +0630

    [Done]env

--- src/firebase.js
+++ src/firebase.js
@@ -7,24 +7,14 @@ import { getStorage } from 'firebase/storage'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.API_KEY,
-//   authDomain: process.env.AUTH_DOMAIN,
-//   databaseURL: process.env.DATABASE_URL,
-//   projectId: process.env.PROJECT_ID,
-//   storageBucket: process.env.STORAGE_BUCKET,
-//   messagingSenderId: process.env.MESSAGING_SENDER_ID,
-//   appId: process.env.APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAjGfqwhM6hqnPK41i5019F1YNX-R28nik",
-  authDomain: "chat-app-b4b9a.firebaseapp.com",
-  databaseURL: "https://chat-app-b4b9a.firebaseio.com",
-  projectId: "chat-app-b4b9a",
-  storageBucket: "chat-app-b4b9a.appspot.com",
-  messagingSenderId: "536813550916",
-  appId: "1:536813550916:web:ad591cba078a634fbaab11"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
