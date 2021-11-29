bc1846c9a23d3bf64b7393bdf4b2274567223db2
kaung-san-hein <kaungsanhein2019@gmail.com>
2021-11-29 14:12:18 +0630

    [Done]Send Message To Selected User

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { db, auth } from "../firebase";
-import { collection, query, where, onSnapshot } from "firebase/firestore";
+import { db, auth, storage } from "../firebase";
+import {
+  collection,
+  query,
+  where,
+  onSnapshot,
+  addDoc,
+  Timestamp,
+} from "firebase/firestore";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
 import User from "../components/User";
+import MessageForm from "../components/MessageForm";
 
 const Home = () => {
   const [users, setUsers] = useState([]);
   const [chat, setChat] = useState(null);
+  const [text, setText] = useState("");
+  const [img, setImg] = useState(null);
+
+  const sender = auth.currentUser.uid;
 
   useEffect(() => {
     const usersRef = collection(db, "users");
     // create query object
-    const q = query(usersRef, where("uid", "not-in", [auth.currentUser.uid]));
+    const q = query(usersRef, where("uid", "not-in", [sender]));
     // execute query
     const unsub = onSnapshot(q, (querySnapshot) => {
       let users = [];
@@ -20,10 +33,39 @@ const Home = () => {
       setUsers(users);
     });
     return () => unsub();
-  }, []);
+  }, [sender]);
 
   const selectUser = (user) => {
     setChat(user);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const receiver = chat.uid;
+    const id =
+      sender > receiver ? `${sender + receiver}` : `${receiver + sender}`;
+
+    let url;
+    if (img) {
+      const imgRef = ref(
+        storage,
+        `images/${new Date().getTime()} - ${img.name}`
+      );
+      const snap = await uploadBytes(imgRef, img);
+      const dlUrl = await getDownloadURL(ref(storage, snap.ref.fullPath));
+      url = dlUrl;
+    }
+
+    await addDoc(collection(db, "messages", id, "chat"), {
+      text,
+      from: sender,
+      to: receiver,
+      createdAt: Timestamp.fromDate(new Date()),
+      media: url || "",
+    });
+    setText("");
+    setImg(null);
   };
 
   return (
@@ -35,9 +77,17 @@ const Home = () => {
       </div>
       <div className="messages_container">
         {chat ? (
-          <div className="messages_user">
-            <h3>{chat.name}</h3>
-          </div>
+          <>
+            <div className="messages_user">
+              <h3>{chat.name}</h3>
+            </div>
+            <MessageForm
+              handleSubmit={handleSubmit}
+              text={text}
+              setText={setText}
+              setImg={setImg}
+            />
+          </>
         ) : (
           <h3 className="no_conv">Select a user to start conversation</h3>
         )}
